b7ab613428e165d1135188cfee1fc5c1afe53fb5
Prabhash <besttlookk@gmail.com>
2022-03-21 00:41:30 +0530

    footer aaded

--- src/components/footer/FooterStyles.js
+++ src/components/footer/FooterStyles.js
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
-  text-align: center;
-  margin-bottom: 1.5rem;
+  position: absolute;
+  bottom: 3rem;
+  left: 50%;
+  transform: translateX(-50%);
   color: white;
+  text-align: center;
 
   a {
     color: var(--color-cyan);
